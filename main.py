912185a2f7df38b72a3f66e25331d255edfdf253
KirillR911 <KirikR911@gmail.com>
2020-02-03 22:28:45 +0300

    new models

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from utils import *
 
 logger = logging.getLogger(__name__)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = torch.load('model.pt', map_location=device)
-detect_oleg(Image.open("881.jpg"), model, device, logger)
+model = torch.load('./models/model.pt', map_location=device)
+detect_oleg(Image.open("0_3002.jpg"), model, device, logger)
+print(classify_face(extract_faces(Image.open("0_3002.jpg"), logger, "lol", device), model, device, logger))
